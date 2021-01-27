cd513e9fcd02fc96420e46e4434f18415a69f6e8
I <roi972@gmail.com>
2021-01-27 23:41:09 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, useState } from "react";
+import { Popover } from "react-tiny-popover";
+import SearchModal from "./SearchModal";
 
 function App() {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="">
+      <header className="header">
+        <Popover
+          isOpen={isPopoverOpen}
+          positions={["bottom"]}
+          padding={5}
+          onClickOutside={() => setIsPopoverOpen(false)}
+          content={<SearchModal searchText={searchText}/>}
         >
-          Learn React
-        </a>
+          <input
+            type="text"
+            className="search"
+            value={searchText}
+            onChange={(e)=>setSearchText(e.target.value)}
+            onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+          ></input>
+        </Popover>
       </header>
+
+      <div>test</div>
     </div>
   );
 }
---
+++ src/SearchModal.js
@@ -0,0 +1,12 @@
+import {React} from 'react'
+import './SearchModal.css'
+
+export default ({searchText}) => {
+return (
+  <div className='sm'>
+   {searchText}
+
+  </div>
+)
+
+}
